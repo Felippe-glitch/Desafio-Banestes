784fa8a4bd4ac9d6fcab49cea43898c5323c0659
Felippe Costa Wernersbach Stein <felps.facul@gmail.com>
2025-04-22 00:44:50 +0000

    fix: carregar componente de forma síncrona para evitar erros

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import ListaClientes from "./components/Clientes/listaClientes.tsx";
 import DetalhesCliente from "./components/Clientes/detalhesClientes.tsx";
+import logo from './components/images/logo_banestes_fundo_azul.jpg';
 
 function App() {
   return (
     <BrowserRouter>
+      <header className="header">
+        <img src={logo} alt="Logo do Banestes" className="logo" />
+      </header>
       <Routes>
         <Route path="/" element={<ListaClientes />} />
         <Route path="/clientes/:id" element={<DetalhesCliente />} />
